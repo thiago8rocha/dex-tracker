f1eb013399fcfbd17def3b787454a75d3ec2e56e
Thiago Rocha <thiago8rocha@gmail.com>
2026-03-27 21:05:16 -0300

    Add missing http import to go_detail_screen

--- lib/screens/go/go_detail_screen.dart
+++ lib/screens/go/go_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pokedex_tracker/models/pokemon.dart';
